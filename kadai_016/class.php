3279de22cca534b8c16d23c802b0bd58f6ade8ce
Aiarashi0616 <160582175+Aiarashi0616@users.noreply.github.com>
2024-07-29 21:09:09 +0900

    Add files via upload

--- kadai_016/class.php
+++ kadai_016/class.php
@@ -10,21 +10,28 @@
      <p>
      <?php
      class Food { 
-     // プロパティを定義する
-     private $name;
-     private $price;
+         // プロパティを定義する
+         private $name;
+         private $price;
 
-     // コンストラクタを定義する
-     public function __construct($name, $price) {
-        $this->name = $name;
-        $this->price = $price;
-     }
+         // コンストラクタを定義する
+         public function __construct($name, $price) {
+            $this->name = $name;
+            $this->price = $price;
+         }
+
+         public function show_price() {
+            echo $this->price . '<br>';
+        }
+
     }
      // インスタンス化する
      $food = new Food('potato', 250);
  
      // インスタンス$foodの各プロパティの値を出力する
      print_r($food);
+
+     $food->show_price();
      ?>
      </p>
 
@@ -32,80 +39,31 @@
      <p>
      <?php
      class Animal { 
-     // プロパティを定義する
-     private $name;
-     private $height;
-     private $weight;
+         // プロパティを定義する
+         private $name;
+         private $height;
+         private $weight;
 
-     // コンストラクタを定義する
-     public function __construct($name, $height,$weight) {
-        $this->name = $name;
-        $this->height = $height;
-        $this->weight = $weight;
-     }
+         // コンストラクタを定義する
+         public function __construct($name, $height,$weight) {
+            $this->name = $name;
+            $this->height = $height;
+            $this->weight = $weight;
+         }
+
+         public function show_height() {
+            echo $this->height . '<br>';
+        }
     }
      // インスタンス化する
      $animal = new Animal('dog',60,5000);
  
      // インスタンス$animalの各プロパティの値を出力する
      print_r($animal);
+
+     $animal->show_height();l
      ?>
      </p>
 
-
-     <p>
-     <?php
-     class Food2 { 
-     // プロパティを定義する
-     private $name;
-     public $price;
-
-      // メソッドを定義する
-     public function set_price(string $price) {
-        $this->price = $price;
-     }
-     public function show_price() {
-        echo $this->price . '<br>';
-    }
-     
-     }
-
-     // インスタンス化する
-     $potato = new Food2();
-
-     // メソッドにアクセスして実行する
-      $potato->set_price('250');
-      $potato->show_price();
-    ?>
-    </p>
-
-
-     <p>
-     <?php
-     class Animal2 { 
-     // プロパティを定義する
-     private $name;
-     public $height;
-     private $weight;
-
-      // メソッドを定義する
-     public function set_height(string $height) {
-        $this->height = $height;
-     }
-     public function show_height() {
-        echo $this->height . '<br>';
-    }
-     
-     }
-
-     // インスタンス化する
-     $dog = new Animal2();
-
-     // メソッドにアクセスして実行する
-      $dog->set_height('60');
-      $dog->show_height();
-  
-     ?>
-     </p>
-
-      
+ </body>
+</html>
